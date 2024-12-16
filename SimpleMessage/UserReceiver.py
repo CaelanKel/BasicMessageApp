3c5cc67b80a6927c0496a2cf7b32de2711291c6a
CaelanKel <150953519+CaelanKel@users.noreply.github.com>
2024-12-16 14:36:27 -0600

    Update UserReceiver.py

--- SimpleMessage/UserReceiver.py
+++ SimpleMessage/UserReceiver.py
@@ -2,7 +2,7 @@ import socket
 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.bind(("192.168.0.169", 5555)) # listen on that ip with that port port must be same as server
+client.bind(("IP your sending too (REPLACE)", 5555)) # listen on that ip with that port port must be same as server
 client.listen(1)
 running = True
 while running:
